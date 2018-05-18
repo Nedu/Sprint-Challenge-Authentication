5c269f86a0a02a9047923411930e44b17c6d9250
Nedu Robert <nedumrobert@gmail.com>
2018-05-18 18:41:23 +0100

    added Home component

---
+++ client/src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { styles } from '../styles';
+
+const Home = () => {
+    return (
+        <div style={styles.HomeContainer}>
+            <h1 style={styles.HomeFont}>Jokes</h1>
+        </div>
+    );
+};
+
+export default Home;
