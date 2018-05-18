f0cb93869526599888a41e342aefd1dd6b40a71a
Nedu Robert <nedumrobert@gmail.com>
2018-05-18 18:43:16 +0100

    added styling

---
+++ client/src/styles.js
@@ -0,0 +1,34 @@
+const HomeFont = {
+	fontFamily: 'Orbitron',
+	fontSize: '70px',
+	fontStyle: 'italic',
+	fontVariant: 'small-caps',
+    fontWeight: '700',
+    margin: 0
+}
+
+const HomeContainer = {
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+}
+
+const UserForm = {
+    width: '500px',
+    margin: '50px auto'
+}
+
+const CardLayout = {
+    marginTop: '50px',
+    // justifyContent: 'center',
+    maxWidth: '300px'
+}
+
+export const styles = {
+    HomeFont,
+    HomeContainer,
+    UserForm,
+    CardLayout
+}
